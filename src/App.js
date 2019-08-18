c50e1f536911e365b7c25618c3fbf64add430376
Kimmo Halonen <hemmoi@gmail.com>
2019-08-18 21:22:59 +0300

    Router added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import BugListing from './pages/BugListing'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={BugListing} />
+      <Route path="/dashboard" component={BugListing} />
+    </Router>
   );
 }
 
