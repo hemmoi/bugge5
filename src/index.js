36675b2b31bc2fabeda99ca8d9f7a7d2011ff070
Kimmo Halonen <hemmoi@gmail.com>
2019-08-18 20:53:24 +0300

    Material UI added

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store/configureStore';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 
 const preloadedState = window.__PRELOADED_STATE__
 const store = configureStore(preloadedState)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root')
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </ThemeProvider>
+    , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
